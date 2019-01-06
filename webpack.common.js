e6f213c16e5c04cf943eb45719a551dd59ef30ba
cbowen <cbowen@cse.msu.edu>
2019-01-06 13:03:07 -0500

    New cl-publish system.

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,7 @@
 const path = require('path');
-const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
 	entry: {
 		FileSystemConsole: path.resolve(__dirname, 'js/Console/index.js')
-	},
-	plugins: [
-		new FileManagerPlugin({
-			onEnd: {
-				copy: [
-					{source: path.resolve(__dirname, '../../../cl/dist/filesystemconsole.*'), destination: path.resolve(__dirname, 'cldist') }
-				]
-			}
-		})
-	]
+	}
 }
